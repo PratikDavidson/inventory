c98a6f5f1dc70b7d565266ab94ac7a2b000ddf73
PratikDavidson <mcknight.m134@gmail.com>
2023-01-18 15:22:18 +0530

    Inventory Managment System

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from './Info.js'
+import {PropTypes} from 'prop-types'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <AddItem text='2' />
     </div>
   );
+}
+
+
+
+function AddItem(props) {
+  return (
+    <form>
+      <label for='text-form'>Type Something:</label>
+      <input type='text' id='text-form' value={ props.text } />
+    </form>
+  )
+}
+
+AddItem.defaultProps = {
+  text: 'hello'
+}
+
+AddItem.propTypes = {
+  text: PropTypes.string
 }
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Info extends React.Component{
+
+    render() {
+        return (
+            <div>
+              <h1>Inventory Management</h1>
+              <p>Manage your stuffs!</p>
+            </div>
+          )
+    }
+}
+
+
+export default Info;
